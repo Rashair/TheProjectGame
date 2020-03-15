3e13d60c1ee43c71258c4b160a6d147ad4999603
Przemysław Wiczołek <przemyslaw.wk@gmail.com>
2020-03-15 23:05:38 +0100

    Add missing configuration fields

--- GameMaster/Models/Configuration.cs
+++ GameMaster/Models/Configuration.cs
@@ -10,8 +10,10 @@ namespace GameMaster.Models
         public readonly TimeSpan putPenalty;
         public readonly TimeSpan checkPenalty;
         public readonly TimeSpan responsePenalty;
-        public readonly int x;
-        public readonly int y;
+        public readonly int width;
+        public readonly int height;
         public readonly int numberOfGoals;
+        public readonly int goalAreaHeight;
+        public readonly int shamPieceProbability; // percentage
     }
 }
