0a4ba44cee697d4e50b3c165629086d1cee690b3
Anna Szmurło <01130615@pw.edu.pl>
2020-03-16 01:08:17 +0100

    Added types of messages, and logic for JoinTheGame() function

---
+++ Player/Models/Messages/Message.cs
@@ -0,0 +1,42 @@
+﻿using Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Player.Models.Messages
+{
+    public class Message
+    {
+        public int messageID;
+    }
+
+    public class JoinGameRequestMessage: Message
+    {
+        public string teamID;
+    }
+
+    public class RegularMessage: Message
+    {
+        public int agentID;
+        public int payload;
+    }
+
+    public class MoveMessage: RegularMessage
+    {
+        public string direction;
+    }
+
+    public class BegForInfoMessage: RegularMessage
+    {
+        public int askedAgentID;
+    }
+
+    public class GiveInfoMessage: RegularMessage
+    {
+        public int respondToID;
+        public int[,] distances;
+        public GoalAreaInformations[,] redTeamGoalAreaInformations;
+        public GoalAreaInformations[,] blueTeamGoalAreaInformations;
+    }
+}
