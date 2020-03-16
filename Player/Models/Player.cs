22ad7ce736a028049e9446f0524ae3dfe93fa4f2
Anna Szmurło <01130615@pw.edu.pl>
2020-03-16 12:24:23 +0100

    Added all player methods (without Communicate() and AcceptMessage())

--- Player/Models/Player.cs
+++ Player/Models/Player.cs
@@ -1,4 +1,4 @@
-﻿using Newtonsoft.Json;
+using Newtonsoft.Json;
 using Player.Clients;
 using Player.Models.Messages;
 using Player.Models.Payloads;
@@ -8,6 +8,7 @@ using Shared.Models.Messages;
 using Shared.Senders;
 using System;
 using System.Collections.Generic;
+using System.Threading;
 using System.Threading.Tasks.Dataflow;
 
 namespace Player.Models
@@ -26,6 +27,8 @@ namespace Player.Models
         private IStrategy strategy;
         public int[] teamMates;
         public (int x, int y) boardSize;
+        private bool working;
+        private int leaderId;
 
         private Penalties penaltiesTimes;
         private BufferBlock<GMMessage> queue;
@@ -51,12 +54,16 @@ namespace Player.Models
 
         public async void Start()
         {
-            throw new NotImplementedException();
+            working = true;
+            while(working)
+            {
+                Penalty();
+            }
         }
 
         public void Stop()
         {
-            throw new NotImplementedException();
+            working = false;
         }
 
         public void Move(Directions _direction)
@@ -68,6 +75,7 @@ namespace Player.Models
                 //payload = ,
                 direction = _direction.ToString()
             };
+            Communicate(message);
         }
 
         public void Put()
@@ -77,6 +85,7 @@ namespace Player.Models
                 messageID = (int)MessageID.Put,
                 agentID = id
             };
+            Communicate(message);
         }
 
         public void BegForInfo()
@@ -107,12 +116,18 @@ namespace Player.Models
             {
                 messageID = (int)MessageID.GiveInfo,
                 agentID = id,
-                //payload = ,
-                respondToID = waitingPlayers[0],
+                //payload = ,              
                 distances = new int[boardSize.x, boardSize.y],
                 redTeamGoalAreaInformations = new GoalInfo[boardSize.x, boardSize.y],
                 blueTeamGoalAreaInformations = new GoalInfo[boardSize.x, boardSize.y],
             };
+            if (toLeader)
+                message.respondToID = leaderId;
+            else
+            {
+                message.respondToID = waitingPlayers[0];
+                waitingPlayers.RemoveAt(0);
+            }
 
             for (int i = 0; i <board.Length; ++i)
             {
@@ -136,7 +151,12 @@ namespace Player.Models
 
         public void RequestsResponse(int respondToID, bool _isLeader = false)
         {
-            throw new NotImplementedException();
+            if (_isLeader)
+            {
+                GiveInfo(_isLeader);
+            }
+            else
+                waitingPlayers.Add(respondToID);
         }
 
         public void CheckPiece()
@@ -257,7 +277,7 @@ namespace Player.Models
         
         public void MakeDecisionFromStrategy()
         {
-            throw new NotImplementedException();
+            strategy.MakeDecision(this);
         }
 
         private async void Communicate(Message message)
@@ -267,7 +287,9 @@ namespace Player.Models
 
         private void Penalty()
         {
-            throw new NotImplementedException();
+            Thread.Sleep(penaltyTime);
+            penaltyTime = 0;
+            MakeDecisionFromStrategy();
         }
     }
 }
