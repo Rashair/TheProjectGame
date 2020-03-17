5b80769ed3c4f868caf564cabf65f18af4833d18
Anna Szmurło <01130615@pw.edu.pl>
2020-03-17 16:33:06 +0100

    Fill the game configuration form by default values read from file

--- GameMaster/ClientApp/src/components/Home.js
+++ GameMaster/ClientApp/src/components/Home.js
@@ -1,80 +1,112 @@
 ﻿import React, { Component } from 'react';
+import gameConfigData from  './gameConfigData';
 
 export class Home extends Component {
+    constructor(props, context) {
+        super(props, context)
+        this.state = {
+            ip: gameConfigData.CsIP,
+            port: gameConfigData.CsPort,
+            movePenalty: gameConfigData.movePenalty,
+            askPenalty: gameConfigData.askPenalty,
+            discoveryPenalty: gameConfigData.discoveryPenalty,
+            putPenalty: gameConfigData.putPenalty,
+            checkForShamPenalty: gameConfigData.checkForShamPenalty,
+            responsePenalty: gameConfigData.responsePenalty,
+            boardX: gameConfigData.boardX,
+            boardY: gameConfigData.boardY,
+            goalAreaHight: gameConfigData.goalAreaHight,
+            numberOfGoals: gameConfigData.numberOfGoals,
+            numberOfPieces: gameConfigData.numberOfPieces,
+        }
+    }
   displayName = Home.name
 
-  render() {
-    return (
-      <form>
+    render() {
+      return (
+          <form >
             <h1>Zmień domyślną konfigurację gry</h1>
 
             <fieldset>
                 <label>Informacje o CS:</label>
                 <fieldset>
-                    <label>IP: </label>
-                    <input type="string" />
+                      <label>IP: </label>
+                      <input type="string" name="ip" value={this.state.ip} 
+                          onChange={e => this.setState({ip: e.target.value })}/>
                 </fieldset>
                 <fieldset>
-                    <label>Port: </label>
-                    <input type="number" />
+                      <label>Port: </label>
+                      <input type="number" name="port" value={this.state.port}
+                          onChange={e => this.setState({ port: e.target.value })} />
                 </fieldset>
             </fieldset>
 
             <fieldset>
                 <label>Opóźnienie w wykonywaniu ruchów przez agenta: </label>
                 <fieldset>
-                    <label>Kara za ruch: </label>
-                    <input type="number" />
+                      <label>Kara za ruch: </label>
+                      <input type="number" name="movePenalty" value={this.state.movePenalty}
+                          onChange={e => this.setState({ movePenalty: e.target.value })} />
                 </fieldset><fieldset>
-                    <label>Kara za prośbę komunikacji: </label>
-                    <input type="number" />
+                      <label>Kara za prośbę komunikacji: </label>
+                      <input type="number" name="askPenalty" value={this.state.askPenalty}
+                          onChange={e => this.setState({ askPenalty: e.target.value })} />
                 </fieldset>
                 <fieldset>
-                    <label>Kara za akcję Discovery: </label>
-                    <input type="number" />
+                      <label>Kara za akcję Discovery: </label>
+                      <input type="number" name="discoveryPenalty" value={this.state.discoveryPenalty}
+                          onChange={e => this.setState({ discoveryPenalty: e.target.value })} />
                 </fieldset>
                 <fieldset>
-                    <label>Kara za odłożenie fragmentu: </label>
-                    <input type="number" />
+                      <label>Kara za odłożenie fragmentu: </label>
+                      <input type="number" name="putPenalty" value={this.state.putPenalty}
+                          onChange={e => this.setState({ putPenalty: e.target.value })} />
                 </fieldset>
                 <fieldset>
-                    <label>Kara za sprawdzenie fragmentu: </label>
-                    <input type="number" />
+                      <label>Kara za sprawdzenie fragmentu: </label>
+                      <input type="number" name="checkForShamePenalty" value={this.state.checkForShamPenalty}
+                          onChange={e => this.setState({ checkForShamPenalty: e.target.value })} />
                 </fieldset>
                 <fieldset>
-                    <label>Kara za odpowiedź: </label>
-                    <input type="number" />
+                      <label>Kara za odpowiedź: </label>
+                      <input type="number" name="responsePenalty" value={this.state.responsePenalty}
+                          onChange={e => this.setState({ responsePenalty: e.target.value })} />
                 </fieldset>
             </fieldset>
 
             <fieldset>
                 <label>Rozmiar planszy:</label>
                 <fieldset>
-                    <label>Wysokość: </label>
-                    <input type="number" />
+                      <label>Wysokość: </label>
+                      <input type="number" name="boardY" value={this.state.boardY}
+                          onChange={e => this.setState({ boardY: e.target.value })} />
                 </fieldset>
                 <fieldset>
-                    <label>Szerokość: </label>
-                    <input type="number" />
+                      <label>Szerokość: </label>
+                      <input type="number" name="boardX" value={this.state.boardX}
+                          onChange={e => this.setState({ boardX: e.target.value })} />
                 </fieldset>
             </fieldset>
 
             <fieldset>
                 <label>Rozmiar pola bramkowego:</label>
                 <fieldset>
-                    <label>Wysokość: </label>
-                    <input type="number" />
+                      <label>Wysokość: </label>
+                      <input type="number" name="goalAreaHeight" value={this.state.goalAreaHight}
+                          onChange={e => this.setState({ goalAreaHight: e.target.value })} />
                 </fieldset>
             </fieldset>
 
             <fieldset>
-                <label>Maksymalna liczba fragmentów na planszy: </label>
-                <input type="number" />
+                  <label>Maksymalna liczba fragmentów na planszy: </label>
+                  <input type="number" name="numberOfPieces" value={this.state.numberOfPieces}
+                      onChange={e => this.setState({ numberOfPieces: e.target.value })} />
             </fieldset>
 
             <fieldset>
-                <label>Ilość celów w polu bramkowym: </label>
-                <input type="number" />
+                  <label>Ilość celów w polu bramkowym: </label>
+                  <input type="number" name="numberOfGoals" value={this.state.numberOfGoals}
+                      onChange={e => this.setState({ numberOfGoals: e.target.value })} />
             </fieldset>
 
             <fieldset>
@@ -90,7 +122,7 @@ export class Home extends Component {
             </fieldset>
             
             <fieldset>
-                <label>Ilość graczy: </label>
+                <label>Ilość graczy w drużynie: </label>
                 <input type="number" />
             </fieldset>
 
