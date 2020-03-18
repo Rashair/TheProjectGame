664255617d1bf9e3bd0d0b3a1f253111b01a025d
Mateusz Zielinski <matzielinski15@gmail.com>
2020-03-18 19:17:00 +0100

    Format code

--- GameMaster/Controllers/WebSocketController.cs
+++ GameMaster/Controllers/WebSocketController.cs
@@ -1,11 +1,10 @@
+using GameMaster.Managers;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Net.WebSockets;
 using System.Threading;
 using System.Threading.Tasks;
-
-using GameMaster.Managers;
 
 namespace GameMaster.Controllers
 {
@@ -14,33 +13,39 @@ namespace GameMaster.Controllers
         private const int _BUFFER_SIZE = 1024 * 4;
         private readonly WebSocketManager<T> _manager;
         public WebSocketManager<T> Manager => _manager;
+
         public WebSocketController(WebSocketManager<T> manager)
         {
             manager = _manager;
         }
+
+        public abstract Task OnMessageAsync(WebSocket socket, WebSocketReceiveResult result, byte[] buffer);
+
         public virtual void OnConnected(WebSocket socket)
         {
             Manager.AddSocket(socket);
         }
+
         public virtual async Task OnDisconnectedAsync(WebSocket socket)
         {
             await Manager.RemoveSocketAsync(Manager.GetId(socket));
         }
+
         public virtual bool AcceptConnection()
         {
             return true;
         }
-        public abstract Task OnMessageAsync(WebSocket socket, WebSocketReceiveResult result, byte[] buffer);
+
         [HttpGet]
         public async Task<IActionResult> Get()
         {
             HttpContext context = ControllerContext.HttpContext;
-            if (!context.WebSockets.IsWebSocketRequest || !AcceptConnection()) 
+            if (!context.WebSockets.IsWebSocketRequest || !AcceptConnection())
                 return BadRequest();
             WebSocket socket = await context.WebSockets.AcceptWebSocketAsync();
             OnConnected(socket);
             byte[] buffer = new byte[_BUFFER_SIZE];
-            WebSocketReceiveResult result = await socket.ReceiveAsync(new ArraySegment<byte>(buffer), 
+            WebSocketReceiveResult result = await socket.ReceiveAsync(new ArraySegment<byte>(buffer),
                 CancellationToken.None);
             while (!result.CloseStatus.HasValue)
             {
--- GameMaster/Managers/WebSocketManager.cs
+++ GameMaster/Managers/WebSocketManager.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace GameMaster.Managers 
+namespace GameMaster.Managers
 {
     public class WebSocketManager<T> : SocketManager<WebSocket, T>
     {
@@ -13,17 +13,19 @@ namespace GameMaster.Managers
         {
             return a == b;
         }
-        protected override async Task CloseSocketAsync(WebSocket socket) 
+
+        protected override async Task CloseSocketAsync(WebSocket socket)
         {
             if (socket.State == WebSocketState.Open)
             {
                 await socket.CloseAsync(
-                    closeStatus: WebSocketCloseStatus.NormalClosure, 
+                    closeStatus: WebSocketCloseStatus.NormalClosure,
                     statusDescription: "Closed by the WebSocketManager",
                     cancellationToken: CancellationToken.None
                 );
             }
         }
+
         protected override async Task SendMessageAsync(WebSocket socket, T message)
         {
             if (socket.State == WebSocketState.Open)
@@ -31,7 +33,7 @@ namespace GameMaster.Managers
                 string serialized = JsonConvert.SerializeObject(message);
                 byte[] buffer = Encoding.UTF8.GetBytes(serialized);
                 await socket.SendAsync(
-                    buffer: new ArraySegment<byte>(buffer, 0, buffer.Length), 
+                    buffer: new ArraySegment<byte>(buffer, 0, buffer.Length),
                     messageType: WebSocketMessageType.Text,
                     endOfMessage: true,
                     cancellationToken: CancellationToken.None
