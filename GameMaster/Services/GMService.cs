3af802e9c75f7a4b8036f2101267207dccf518de
Przemysław Wiczołek <przemyslaw.wk@gmail.com>
2020-03-19 19:12:49 +0100

    Add template for GM service

---
+++ GameMaster/Services/GMService.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GameMaster.Services
+{
+    public class GMService : IHostedService
+    { 
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
