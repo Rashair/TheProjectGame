d73e3d36b58a5e8017e9ba6db7b77bf9e6a35ad5
Anna Szmurło <01130615@pw.edu.pl>
2020-03-21 03:31:45 +0100

    Add saving info about game results in log file

--- IntegrationTests/UnitTest1.cs
+++ IntegrationTests/UnitTest1.cs
@@ -1,3 +1,6 @@
+using Shared;
+using System;
+using System.IO;
 using Xunit;
 
 namespace IntegrationTests
@@ -5,8 +8,19 @@ namespace IntegrationTests
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void TestCreateLogfile()
         {
+            // Arrange
+            var logger = new Logger();
+            string str = "Hello!";
+            string path = "C:/Log/";
+            string fileName = DateTime.Now.Day.ToString() + DateTime.Now.Month.ToString() + DateTime.Now.Year.ToString() + "_Logs.txt";
+
+            // Act
+            logger.Log(str);
+            bool created = File.Exists(path + fileName);
+            // Assert 
+            Assert.True(created);
         }
     }
 }
