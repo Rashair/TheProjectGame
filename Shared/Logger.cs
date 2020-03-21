792f29e7d1ebaddb3d9d90f799271e82eaa68fd2
Anna Szmurło <01130615@pw.edu.pl>
2020-03-21 03:31:45 +0100

    Add saving info about game results in log file

---
+++ Shared/Logger.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Shared
+{
+    public class Logger
+    {
+        public static void VerifyDir(string path)
+        {
+            try
+            {
+                DirectoryInfo dir = new DirectoryInfo(path);
+                if (!dir.Exists)
+                {
+                    dir.Create();
+                }
+            }
+            catch
+            {
+            }
+        }
+
+        public void Log(string lines)
+        {
+            string path = "C:/Log/";
+            VerifyDir(path);
+            string fileName = DateTime.Now.Day.ToString() + DateTime.Now.Month.ToString() + DateTime.Now.Year.ToString() + "_Logs.txt";
+            try
+            {
+                System.IO.StreamWriter file = new System.IO.StreamWriter(path + fileName, true);
+                file.WriteLine(DateTime.Now.ToString() + '\n' + lines);
+                file.Close();
+            }
+            catch (Exception)
+            {
+            }
+        }
+    }
+}
