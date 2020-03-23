81e925ee20154ca80fb8b208c7c23d04d39bbc44
Przemysław Wiczołek <przemyslaw.wk@gmail.com>
2020-03-23 20:20:28 +0100

    Add public-fields rules and fix

--- Player/Models/Field.cs
+++ Player/Models/Field.cs
@@ -4,8 +4,10 @@ namespace Player.Models
 {
     public class Field
     {
-        public GoalInfo goalInfo;
-        public bool playerInfo;
-        public int distToPiece;
+        public GoalInfo GoalInfo { get; set; }
+
+        public bool PlayerInfo { get; set; }
+
+        public int DistToPiece { get; set; }
     }
 }
