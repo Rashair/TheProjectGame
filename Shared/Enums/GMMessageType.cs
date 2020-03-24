9362f55415d8dda3805a1fb5333564e2a7e4ab53
Anna Szmurło <01130615@pw.edu.pl>
2020-03-24 16:51:25 +0100

    Organize message types

---
+++ Shared/Enums/GMMessageType.cs
@@ -0,0 +1,17 @@
+﻿namespace Shared.Enums
+{
+    public enum GMMessageType
+    {
+        CheckPieceResponse = 101,
+        PieceDestructionResponse,
+        DiscoverResponse,
+        EndOfGame,
+        StartOfGame,
+        ForwardKnowledgeReply,
+        JoinTheGameResponse,
+        MoveResponse,
+        PickResponse,
+        PutResponse,
+        ForwardKnowledgeQuestion,
+    }
+}
