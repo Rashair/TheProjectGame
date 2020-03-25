94d1345d2aaa21adfecd5b6469dbc349da4a45df
Mateusz Zielinski <matzielinski15@gmail.com>
2020-03-25 02:58:03 +0100

    Add ISocketManager

---
+++ GameMaster/Managers/ISocketManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GameMaster.Managers
+{
+    public interface ISocketManager<TSocket, TMessage>
+    {
+        string GetId(TSocket socket);
+
+        TSocket GetSocketById(string id);
+
+        bool AddSocket(TSocket socket);
+
+        Task<bool> RemoveSocketAsync(string id, CancellationToken cancellationToken);
+
+        Task SendMessageAsync(string id, TMessage message, CancellationToken cancellationToken);
+
+        Task SendMessageToAllAsync(TMessage message, CancellationToken cancellationToken);
+    }
+}
