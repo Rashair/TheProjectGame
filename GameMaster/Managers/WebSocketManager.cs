ca0608e6f2e5ffb59f264c3cd98097bfce8c89f5
Mateusz Zielinski <matzielinski15@gmail.com>
2020-03-25 03:16:44 +0100

    Add CancellationToken to WebSocketManager

--- GameMaster/Managers/WebSocketManager.cs
+++ GameMaster/Managers/WebSocketManager.cs
@@ -8,27 +8,28 @@ using Newtonsoft.Json;
 
 namespace GameMaster.Managers
 {
-    public class WebSocketManager<T> : SocketManager<WebSocket, T>
+    public class WebSocketManager<TMessage> : SocketManager<WebSocket, TMessage>
     {
         protected override bool IsSame(WebSocket a, WebSocket b)
         {
             return a == b;
         }
 
-        protected override async Task CloseSocketAsync(WebSocket socket)
+        protected override async Task CloseSocketAsync(WebSocket socket, CancellationToken cancellationToken)
         {
             if (socket.State == WebSocketState.Open)
             {
                 await socket.CloseAsync(
                     closeStatus: WebSocketCloseStatus.NormalClosure,
                     statusDescription: "Closed by the WebSocketManager",
-                    cancellationToken: CancellationToken.None);
+                    cancellationToken: cancellationToken);
             }
         }
 
-        protected override async Task SendMessageAsync(WebSocket socket, T message)
+        protected override async Task SendMessageAsync(WebSocket socket, TMessage message,
+            CancellationToken cancellationToken)
         {
-            if (socket.State == WebSocketState.Open)
+            if (!cancellationToken.IsCancellationRequested && (socket.State == WebSocketState.Open))
             {
                 string serialized = JsonConvert.SerializeObject(message);
                 byte[] buffer = Encoding.UTF8.GetBytes(serialized);
@@ -36,7 +37,7 @@ namespace GameMaster.Managers
                     buffer: new ArraySegment<byte>(buffer, 0, buffer.Length),
                     messageType: WebSocketMessageType.Text,
                     endOfMessage: true,
-                    cancellationToken: CancellationToken.None);
+                    cancellationToken: cancellationToken);
             }
         }
     }
