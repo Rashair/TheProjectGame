d2f4e688d3b02bec294d8ddbcced40d7b69d48d2
Przemysław Wiczołek <przemyslaw.wk@gmail.com>
2020-03-26 19:07:37 +0100

    Add extension test

--- SharedTests/Enums/DirectionExtensionsTests.cs
+++ SharedTests/Enums/DirectionExtensionsTests.cs
@@ -1,4 +1,5 @@
-﻿using Xunit;
+﻿using Shared.Enums;
+using Xunit;
 
 namespace SharedTests
 {
@@ -7,6 +8,23 @@ namespace SharedTests
         [Fact]
         public void TestGetCoordinatesAroundCenterMiddleField()
         {
+            // Arrange
+            (int y, int x) center = (5, 4);
+
+            // Act
+            var result = DirectionExtensions.GetCoordinatesAroundCenter(center);
+
+            // Assert
+            Assert.Equal(9, result.Length);
+            Assert.Contains((Direction.NW, center.y - 1, center.x - 1), result);
+            Assert.Contains((Direction.N, center.y - 1, center.x), result);
+            Assert.Contains((Direction.NE, center.y - 1, center.x + 1), result);
+            Assert.Contains((Direction.W, center.y, center.x - 1), result);
+            Assert.Contains((Direction.FromCurrent, center.y, center.x), result);
+            Assert.Contains((Direction.E, center.y, center.x + 1), result);
+            Assert.Contains((Direction.SW, center.y + 1, center.x - 1), result);
+            Assert.Contains((Direction.S, center.y + 1, center.x), result);
+            Assert.Contains((Direction.SE, center.y + 1, center.x + 1), result);
         }
     }
 }
