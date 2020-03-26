4426229ed38fb427adfcadc023a92c3dab6494b3
Przemysław Wiczołek <przemyslaw.wk@gmail.com>
2020-03-26 23:43:25 +0100

    Move to new template

--- GameMaster/ClientApp/src/App.js
+++ GameMaster/ClientApp/src/App.js
@@ -3,14 +3,14 @@ import { Route } from 'react-router';
 import { Layout } from './components/Layout';
 import { Home } from './components/Home';
 
-export default class App extends Component {
-  displayName = App.name
+import './custom.css'
 
-  render() {
-    return (
-      <Layout>
-        <Route exact path='/' component={Home} />
-      </Layout>
-    );
-  }
+export default class App extends Component {
+    render() {
+        return (
+            <Layout>
+                <Route exact path='/' component={Home} />
+            </Layout>
+        );
+    }
 }
