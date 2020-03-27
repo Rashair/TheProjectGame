59e221caf125cb28d4eee64007e49a6e0d8844fb
Anna Szmurło <01130615@pw.edu.pl>
2020-03-27 11:30:58 +0100

    Add Serilog logger and delete old one

--- GameMaster/Startup.cs
+++ GameMaster/Startup.cs
@@ -63,8 +63,10 @@ namespace GameMaster
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
+            loggerFactory.AddSerilog();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
