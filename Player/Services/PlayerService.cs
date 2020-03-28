0a8027ef0b1db2f6d87c9a5870946ae9f8a4cd21
Bartek <bartek77@live.com>
2020-03-28 17:15:00 +0100

    Add PlayerService tests

--- Player/Services/PlayerService.cs
+++ Player/Services/PlayerService.cs
@@ -2,7 +2,6 @@
 using System.Threading.Tasks;
 
 using Microsoft.Extensions.Hosting;
-using Player.Models;
 
 namespace Player.Services
 {
@@ -10,17 +9,27 @@ namespace Player.Services
     {
         private readonly Models.Player player;
 
+        public int WaitForInitializeDelay { get; private set; }
+
         public PlayerService(Models.Player player)
         {
             this.player = player;
+            WaitForInitializeDelay = 100;
         }
 
         protected async override Task ExecuteAsync(CancellationToken cancellationToken)
         {
+            await WaitForInitialization(cancellationToken);
+
             if (!cancellationToken.IsCancellationRequested)
             {
                 await Task.Run(() => player.Work(cancellationToken));
             }
+        }
+
+        private async Task WaitForInitialization(CancellationToken cancellationToken)
+        {
+            await Task.Delay(WaitForInitializeDelay, cancellationToken);
         }
     }
 }
