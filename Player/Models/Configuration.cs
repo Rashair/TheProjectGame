8855d84549b90ea23bb97e60988127f23d0b06ab
Bartek <bartek77@live.com>
2020-03-28 20:34:26 +0100

    Add Configuration in Player

---
+++ Player/Models/Configuration.cs
@@ -0,0 +1,18 @@
+﻿using Player.Models.Strategies;
+using Shared.Enums;
+
+namespace Player.Models
+{
+    public class Configuration
+    {
+        public Team Team { get; set; }
+
+        public IStrategy Strategy { get; set; }
+
+        public void Update(Configuration conf)
+        {
+            Team = conf.Team;
+            Strategy = conf.Strategy;
+        }
+    }
+}
