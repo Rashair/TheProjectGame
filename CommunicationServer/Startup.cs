1b230f55410aa5b8ee581909bd8931521ba6b035
Anna Szmurło <01130615@pw.edu.pl>
2020-03-28 11:43:43 +0100

    Add Serilog to communicationServer project

--- CommunicationServer/Startup.cs
+++ CommunicationServer/Startup.cs
@@ -1,12 +1,34 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using System;
+
+using static System.Environment;
+
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+using Serilog.Events;
 
 namespace CommunicationServer
 {
     public class Startup
     {
+        public Startup()
+        {
+            Log.Logger = new LoggerConfiguration()
+               .Enrich.FromLogContext()
+               .WriteTo.File(
+               path: GetFolderPath(SpecialFolder.MyDocuments) + "\\Log\\theProjectGame_CommunicationServer_" + DateTime.Today.Day.ToString()
+               + "_" + DateTime.Today.Month.ToString()
+               + "_" + DateTime.Today.Year.ToString() + ".txt",
+               rollOnFileSizeLimit: true,
+               outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} {SourceContext}{NewLine}[{Level}] {Message}{NewLine}{Exception}")
+                .MinimumLevel.Information()
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+                .MinimumLevel.Override("System", LogEventLevel.Warning)
+               .CreateLogger();
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
--- GameMaster/Startup.cs
+++ GameMaster/Startup.cs
@@ -62,7 +62,7 @@ namespace GameMaster
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
             {
