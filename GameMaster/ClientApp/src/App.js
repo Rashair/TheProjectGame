659ee75a897b23fab8e20be0dc7de734a0d5db1e
Przemysław Wiczołek <przemyslaw.wk@gmail.com>
2020-03-28 01:21:39 +0100

    Add start page

--- GameMaster/ClientApp/src/App.js
+++ GameMaster/ClientApp/src/App.js
@@ -1,16 +1,18 @@
-import React, { Component } from 'react';
-import { Route } from 'react-router';
-import { Layout } from './components/Layout';
-import { Home } from './components/Home';
+import React, { Component } from "react";
+import { Route } from "react-router";
+import { Layout } from "./components/Layout";
+import { Home } from "./components/Home";
+import { Start } from "./components/Start";
 
-import './custom.css'
+import "./custom.css";
 
 export default class App extends Component {
-    render() {
-        return (
-            <Layout>
-                <Route exact path='/' component={Home} />
-            </Layout>
-        );
-    }
+  render() {
+    return (
+      <Layout>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/start" component={Start} />
+      </Layout>
+    );
+  }
 }
---
+++ GameMaster/ClientApp/src/components/Start.js
@@ -0,0 +1,29 @@
+﻿import React, { Component } from "react";
+
+const error = e => {
+  alert("Coś poszło nie tak, spróbuj ponownie.");
+  console.log(`Error: ${e}`);
+};
+
+export class Start extends Component {
+  constructor(props, context) {
+    super(props, context);
+    this.state = {};
+    this.startGame = this.startGame.bind(this);
+  }
+
+  startGame(e) {
+    const button = e.target;
+    button.disabled = true;
+  }
+  componentDidMount() {}
+
+  render() {
+    return (
+      <div className="text-center">
+        <h1 className="mb-5">Start gry</h1>
+        <input className="btn btn-success btn-lg p-3" type="submit" value="Start" onClick={this.startGame} />
+      </div>
+    );
+  }
+}
