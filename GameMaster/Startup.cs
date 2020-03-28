4c9040570b7b09654c2d725b005130dff14d77e6
Anna Szmurło <01130615@pw.edu.pl>
2020-03-28 11:43:43 +0100

    Add Serilog to communicationServer project

--- GameMaster/Startup.cs
+++ GameMaster/Startup.cs
@@ -63,7 +63,7 @@ namespace GameMaster
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
             {
