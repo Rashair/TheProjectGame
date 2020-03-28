10954c31f01cbc383ae96a88534a72b1946f1434
Anna Szmurło <01130615@pw.edu.pl>
2020-03-28 11:43:43 +0100

    Add Serilog to communicationServer project

--- CommunicationServer/Program.cs
+++ CommunicationServer/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Serilog;
 
 namespace CommunicationServer
 {
@@ -12,6 +13,7 @@ namespace CommunicationServer
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseSerilog()
                 .UseStartup<Startup>();
     }
 }
