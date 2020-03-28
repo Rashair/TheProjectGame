3ae052e26dce29382b19a1762cad8d2df43da2a0
Anna Szmurło <01130615@pw.edu.pl>
2020-03-28 11:36:34 +0100

    Add Serilog to Player project

--- Player/Startup.cs
+++ Player/Startup.cs
@@ -1,4 +1,7 @@
-﻿using System.Threading.Tasks.Dataflow;
+﻿using System;
+using System.Threading.Tasks.Dataflow;
+
+using static System.Environment;
 
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -6,12 +9,30 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Player.Clients;
 using Player.Services;
+using Serilog;
+using Serilog.Events;
 using Shared.Messages;
 
 namespace Player
 {
     public class Startup
     {
+        public Startup()
+        {
+            Log.Logger = new LoggerConfiguration()
+               .Enrich.FromLogContext()
+               .WriteTo.File(
+               path: GetFolderPath(SpecialFolder.MyDocuments) + "\\Log\\theProjectGame_Player_" + DateTime.Today.Day.ToString()
+               + "_" + DateTime.Today.Month.ToString()
+               + "_" + DateTime.Today.Year.ToString() + ".txt",
+               rollOnFileSizeLimit: true,
+               outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} {SourceContext}{NewLine}[{Level}] {Message}{NewLine}{Exception}")
+                .MinimumLevel.Information()
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+                .MinimumLevel.Override("System", LogEventLevel.Warning)
+               .CreateLogger();
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
