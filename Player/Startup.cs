ec506a3d28401486a249118520048f8ef89d8280
Bartek <bartek77@live.com>
2020-03-28 15:50:25 +0100

    Implement player service

--- Player/Startup.cs
+++ Player/Startup.cs
@@ -20,6 +20,7 @@ namespace Player
             services.AddSingleton<BufferBlock<GMMessage>>();
 
             services.AddHostedService<SocketService>();
+            services.AddHostedService<PlayerService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
