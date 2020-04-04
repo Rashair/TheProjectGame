0e45ce8a2b3d3b92bf95313c837a6c471a6e8aac
Przemysław Wiczołek <przemyslaw.wk@gmail.com>
2020-04-04 12:50:10 +0200

    Add basic Player start test

---
+++ IntegrationTests/StartTests.cs
@@ -0,0 +1,35 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+using Player.Services;
+using Shared;
+using Xunit;
+
+namespace IntegrationTests
+{
+    public class StartTests
+    {
+        [Fact(Timeout = 1500)]
+        public async void PlayerStarts()
+        {
+            // Arrange
+            var source = new CancellationTokenSource();
+            string[] args = new string[] { "TeamID=red" };
+            var webhost = Player.Program.CreateWebHostBuilder(args).Build();
+
+            // Act
+            await webhost.StartAsync(source.Token);
+            await Task.Delay(500);
+            source.Cancel();
+
+            // Assert
+            var player = webhost.Services.GetService<Player.Models.Player>();
+            Assert.False(player == null, "Player should not be null");
+
+            // TODO: get hosted services and assert those are not null after move to dotnet-core 3.0
+        }
+    }
+}
