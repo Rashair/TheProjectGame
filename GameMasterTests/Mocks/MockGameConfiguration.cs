5dcccbe8b978639086116dda63839da4c1e322aa
Przemysław Wiczołek <przemyslaw.wk@gmail.com>
2020-04-11 18:23:14 +0200

    Add fields to configuration. Improve coping properties. Improve tests

--- GameMasterTests/Mocks/MockGameConfiguration.cs
+++ GameMasterTests/Mocks/MockGameConfiguration.cs
@@ -10,10 +10,12 @@ namespace GameMaster.Tests.Mocks
             this.CsPort = 3729;
             this.MovePenalty = 1500;
             this.AskPenalty = 1000;
+            this.ResponsePenalty = 1000;
             this.DiscoverPenalty = 700;
             this.PutPenalty = 500;
             this.CheckPenalty = 700;
-            this.ResponsePenalty = 1000;
+            this.DestroyPenalty = 100;
+            this.PickPenalty = 100;
             this.GoalAreaHeight = 5;
             this.Height = 12;
             this.Width = 10;
