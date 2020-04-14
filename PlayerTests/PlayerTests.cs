319a1f72cea2cd9c1bab370a0375870d437117ce
Anna Szmurło <01130615@pw.edu.pl>
2020-04-14 16:48:09 +0200

    Add Player AcceptMessage tests

--- PlayerTests/PlayerTests.cs
+++ PlayerTests/PlayerTests.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Threading.Tasks.Dataflow;
@@ -5,7 +6,6 @@ using System.Threading.Tasks.Dataflow;
 using Newtonsoft.Json;
 using Player.Clients;
 using Player.Models;
-using Player.Models.Strategies;
 using Shared.Enums;
 using Shared.Messages;
 using Shared.Models;
@@ -120,6 +120,313 @@ namespace Player.Tests
             await player.AcceptMessage(CancellationToken.None);
 
             Assert.Single(player.WaitingPlayers);
+        }
+
+        [Fact]
+        public async Task TestAcceptMessageCheckAnswerShouldChangeIsHeldPieceShamToTrue()
+        {
+            // Arrange
+            CheckAnswerPayload payloadCheck = new CheckAnswerPayload()
+            {
+               Sham = true,
+            };
+            GMMessage messageCheck = new GMMessage()
+            {
+                Id = GMMessageID.CheckAnswer,
+                Payload = JsonConvert.SerializeObject(payloadCheck),
+            };
+
+            StartGamePayload payloadStart = new StartGamePayload
+            {
+                PlayerID = 1,
+                AlliesIDs = new int[1] { 2 },
+                LeaderID = 1,
+                EnemiesIDs = new int[2] { 3, 4 },
+                TeamId = Team.Red,
+                BoardSize = new BoardSize { X = 3, Y = 3 },
+                GoalAreaSize = 1,
+                NumberOfPlayers = new NumberOfPlayers { Allies = 2, Enemies = 2 },
+                NumberOfPieces = 2,
+                NumberOfGoals = 2,
+                Penalties = new Penalties { Move = "0", CheckForSham = "0", Discovery = "0", DestroyPiece = "0", PutPiece = "0", InformationExchange = "0" },
+                ShamPieceProbability = 0.5f,
+                Position = new Position { X = 1, Y = 1 },
+            };
+            GMMessage messageStart = new GMMessage()
+            {
+                Id = GMMessageID.StartGame,
+                Payload = JsonConvert.SerializeObject(payloadStart),
+            };
+
+            BufferBlock<GMMessage> input = new BufferBlock<GMMessage>();
+            input.Post<GMMessage>(messageStart);
+            input.Post<GMMessage>(messageCheck);
+
+            PlayerConfiguration configuration = new PlayerConfiguration() { CsIP = "192.168.0.0", CsPort = 3729, TeamID = "red", Strategy = 3 };
+            var player = new Player.Models.Player(configuration, input, new WebSocketClient<GMMessage, PlayerMessage>());
+
+            // Act
+            bool expectedResult = true;
+
+            await player.AcceptMessage(CancellationToken.None);
+            await player.AcceptMessage(CancellationToken.None);
+            bool? result = player.IsHeldPieceSham;
+
+            // Assert
+            Assert.Equal(expectedResult, result);
+        }
+
+        [Fact]
+        public async Task TestAcceptMessageDestructionAnswerShouldDestroyHoldingPiece()
+        {
+            // Arrange
+            EmptyAnswerPayload destructionPayload = new EmptyAnswerPayload();
+            GMMessage destructionMessage = new GMMessage(GMMessageID.DestructionAnswer, destructionPayload);
+
+            EmptyAnswerPayload pickPayload = new EmptyAnswerPayload();
+            GMMessage pickMessage = new GMMessage(GMMessageID.PickAnswer, pickPayload);
+
+            StartGamePayload payloadStart = new StartGamePayload
+            {
+                PlayerID = 1,
+                AlliesIDs = new int[1] { 2 },
+                LeaderID = 1,
+                EnemiesIDs = new int[2] { 3, 4 },
+                TeamId = Team.Red,
+                BoardSize = new BoardSize { X = 3, Y = 3 },
+                GoalAreaSize = 1,
+                NumberOfPlayers = new NumberOfPlayers { Allies = 2, Enemies = 2 },
+                NumberOfPieces = 2,
+                NumberOfGoals = 2,
+                Penalties = new Penalties { Move = "0", CheckForSham = "0", Discovery = "0", DestroyPiece = "0", PutPiece = "0", InformationExchange = "0" },
+                ShamPieceProbability = 0.5f,
+                Position = new Position { X = 1, Y = 1 },
+            };
+            GMMessage startMessage = new GMMessage()
+            {
+                Id = GMMessageID.StartGame,
+                Payload = JsonConvert.SerializeObject(payloadStart),
+            };
+
+            BufferBlock<GMMessage> input = new BufferBlock<GMMessage>();
+            input.Post<GMMessage>(startMessage);
+            input.Post<GMMessage>(pickMessage);
+            input.Post<GMMessage>(destructionMessage);
+
+            PlayerConfiguration configuration = new PlayerConfiguration() { CsIP = "192.168.0.0", CsPort = 3729, TeamID = "red", Strategy = 3 };
+            var player = new Player.Models.Player(configuration, input, new WebSocketClient<GMMessage, PlayerMessage>());
+
+            // Act
+            bool expectedHasPieceValue = false;
+
+            await player.AcceptMessage(CancellationToken.None);
+            await player.AcceptMessage(CancellationToken.None);
+            await player.AcceptMessage(CancellationToken.None);
+            bool realHasPieceValue = player.HasPiece;
+            bool? realIsHeldPieceShamValue = player.IsHeldPieceSham;
+
+            // Assert
+            Assert.Equal(expectedHasPieceValue, realHasPieceValue);
+            Assert.Null(realIsHeldPieceShamValue);
+        }
+
+        [Fact]
+        public async Task TestAcceptMessageStartGameShouldSetFields()
+        {
+            int playerId = 1;
+            int leaderId = 1;
+            Team teamId = Team.Red;
+            int[] alliesId = new int[1] { 2 };
+            int[] enemiesId = new int[2] { 3, 4 };
+            BoardSize boardSize = new BoardSize { X = 3, Y = 3 };
+            int goalAreaSize = 1;
+            NumberOfPlayers numberOfPlayers = new NumberOfPlayers { Allies = 2, Enemies = 2 };
+            int numberOfPieces = 2;
+            int numberOfGoals = 2;
+            Penalties penalties = new Penalties { Move = "0", CheckForSham = "0", Discovery = "0", DestroyPiece = "0", PutPiece = "0", InformationExchange = "0" };
+            float shanProbability = 0.5f;
+            Position position = new Position { X = 1, Y = 1 };
+
+            // Arrange
+            StartGamePayload startGamePayload = new StartGamePayload
+            {
+                PlayerID = playerId,
+                AlliesIDs = alliesId,
+                LeaderID = leaderId,
+                EnemiesIDs = enemiesId,
+                TeamId = teamId,
+                BoardSize = boardSize,
+                GoalAreaSize = goalAreaSize,
+                NumberOfPlayers = numberOfPlayers,
+                NumberOfPieces = numberOfPieces,
+                NumberOfGoals = numberOfGoals,
+                Penalties = penalties,
+                ShamPieceProbability = shanProbability,
+                Position = position,
+            };
+            GMMessage startMessage = new GMMessage(GMMessageID.StartGame, startGamePayload);
+
+            BufferBlock<GMMessage> input = new BufferBlock<GMMessage>();
+            input.Post<GMMessage>(startMessage);
+
+            PlayerConfiguration configuration = new PlayerConfiguration() { CsIP = "192.168.0.0", CsPort = 3729, TeamID = "red", Strategy = 3 };
+            var player = new Player.Models.Player(configuration, input, new WebSocketClient<GMMessage, PlayerMessage>());
+
+            // Act
+            bool expectedisLeader = true;
+            (int, int) expectedBoardSize = (boardSize.X, boardSize.Y);
+            (int, int) expectedPosition = (position.X, position.Y);
+
+            await player.AcceptMessage(CancellationToken.None);
+
+            // TODO check playre id var playerIdResult = player.id;
+            var teamMatesResult = player.TeamMatesIds;
+            var isLeaderResult = player.IsLeader;
+            var teamResult = player.Team;
+            var boardSizeResult = player.BoardSize;
+            var penaltiesResult = player.PenaltiesTimes;
+            var positionResult = player.Position;
+            var enemiesResult = player.EnemiesIDs;
+            var goalAreaSizeResult = player.GoalAreaSize;
+            var numOfPlayersResult = player.NumberOfPlayers;
+            var numOfPiecesResult = player.NumberOfPieces;
+            var numOfGoalsResult = player.NumberOfGoals;
+            var shamProbabilityResult = player.ShamPieceProbability;
+
+            bool realHasPieceValue = player.HasPiece;
+            bool? realIsHeldPieceShamValue = player.IsHeldPieceSham;
+
+            // Assert
+            // Assert.Equal(playerId, playerIdResult); TODO
+            Assert.Equal(alliesId, teamMatesResult);
+            Assert.Equal(expectedisLeader, isLeaderResult);
+            Assert.Equal(teamId, teamResult);
+            Assert.Equal(expectedBoardSize, boardSizeResult);
+            Assert.True(penalties.CheckForSham == penaltiesResult.CheckForSham
+                && penalties.DestroyPiece == penaltiesResult.DestroyPiece
+                && penalties.Discovery == penaltiesResult.DestroyPiece
+                && penalties.InformationExchange == penaltiesResult.InformationExchange
+                && penalties.Move == penaltiesResult.Move
+                && penalties.PutPiece == penaltiesResult.PutPiece);
+            Assert.Equal(expectedPosition, positionResult);
+            Assert.Equal(enemiesId, enemiesResult);
+            Assert.Equal(goalAreaSize, goalAreaSizeResult);
+            Assert.True(numberOfPlayers.Allies == numOfPlayersResult.Allies
+                && numberOfPlayers.Enemies == numOfPlayersResult.Enemies);
+            Assert.Equal(numberOfPieces, numOfPiecesResult);
+            Assert.Equal(numberOfGoals, numOfGoalsResult);
+            Assert.Equal(shanProbability, shamProbabilityResult);
+        }
+
+        [Fact]
+        public async Task TestAcceptMessageMoveAnswerShouldChangePlayerPosition()
+        {
+            // Arrange
+            Position newPosition = new Position()
+            {
+                X = 1,
+                Y = 2,
+            };
+            int distToClosestPiece = 1;
+
+            MoveAnswerPayload moveAnswerPayload = new MoveAnswerPayload()
+            {
+                MadeMove = true,
+                CurrentPosition = newPosition,
+                ClosestPiece = distToClosestPiece,
+            };
+            GMMessage moveAnswerMessage = new GMMessage(GMMessageID.MoveAnswer, moveAnswerPayload);
+
+            StartGamePayload payloadStart = new StartGamePayload
+            {
+                PlayerID = 1,
+                AlliesIDs = new int[1] { 2 },
+                LeaderID = 1,
+                EnemiesIDs = new int[2] { 3, 4 },
+                TeamId = Team.Red,
+                BoardSize = new BoardSize { X = 3, Y = 3 },
+                GoalAreaSize = 1,
+                NumberOfPlayers = new NumberOfPlayers { Allies = 2, Enemies = 2 },
+                NumberOfPieces = 2,
+                NumberOfGoals = 2,
+                Penalties = new Penalties { Move = "0", CheckForSham = "0", Discovery = "0", DestroyPiece = "0", PutPiece = "0", InformationExchange = "0" },
+                ShamPieceProbability = 0.5f,
+                Position = new Position { X = 1, Y = 1 },
+            };
+            GMMessage startMessage = new GMMessage()
+            {
+                Id = GMMessageID.StartGame,
+                Payload = JsonConvert.SerializeObject(payloadStart),
+            };
+
+            BufferBlock<GMMessage> input = new BufferBlock<GMMessage>();
+            input.Post<GMMessage>(startMessage);
+            input.Post<GMMessage>(moveAnswerMessage);
+
+            PlayerConfiguration configuration = new PlayerConfiguration() { CsIP = "192.168.0.0", CsPort = 3729, TeamID = "red", Strategy = 3 };
+            var player = new Player.Models.Player(configuration, input, new WebSocketClient<GMMessage, PlayerMessage>());
+
+            // Act
+            (int, int) expectedPosition = (newPosition.Y, newPosition.X);
+
+            await player.AcceptMessage(CancellationToken.None);
+            await player.AcceptMessage(CancellationToken.None);
+            var realPosition = player.Position;
+            var realDistToClosestPiece = player.Board[player.Position.y, player.Position.x].DistToPiece;
+
+            // Assert
+            Assert.Equal(expectedPosition, realPosition);
+            Assert.Equal(distToClosestPiece, realDistToClosestPiece);
+        }
+
+        [Fact]
+        public async Task TestAcceptMessagePickAnswerShouldChangeDistToPieceAndPickPiece()
+        {
+            EmptyAnswerPayload pickPayload = new EmptyAnswerPayload();
+            GMMessage pickAnswerMessage = new GMMessage(GMMessageID.PickAnswer, pickPayload);
+
+            StartGamePayload payloadStart = new StartGamePayload
+            {
+                PlayerID = 1,
+                AlliesIDs = new int[1] { 2 },
+                LeaderID = 1,
+                EnemiesIDs = new int[2] { 3, 4 },
+                TeamId = Team.Red,
+                BoardSize = new BoardSize { X = 3, Y = 3 },
+                GoalAreaSize = 1,
+                NumberOfPlayers = new NumberOfPlayers { Allies = 2, Enemies = 2 },
+                NumberOfPieces = 2,
+                NumberOfGoals = 2,
+                Penalties = new Penalties { Move = "0", CheckForSham = "0", Discovery = "0", DestroyPiece = "0", PutPiece = "0", InformationExchange = "0" },
+                ShamPieceProbability = 0.5f,
+                Position = new Position { X = 1, Y = 1 },
+            };
+            GMMessage startMessage = new GMMessage()
+            {
+                Id = GMMessageID.StartGame,
+                Payload = JsonConvert.SerializeObject(payloadStart),
+            };
+
+            BufferBlock<GMMessage> input = new BufferBlock<GMMessage>();
+            input.Post<GMMessage>(startMessage);
+            input.Post<GMMessage>(pickAnswerMessage);
+
+            PlayerConfiguration configuration = new PlayerConfiguration() { CsIP = "192.168.0.0", CsPort = 3729, TeamID = "red", Strategy = 3 };
+            var player = new Player.Models.Player(configuration, input, new WebSocketClient<GMMessage, PlayerMessage>());
+
+            // Act
+            bool expectedHasPieceValue = true;
+            var expectedDistance = int.MaxValue;
+
+            await player.AcceptMessage(CancellationToken.None);
+            await player.AcceptMessage(CancellationToken.None);
+
+            var realHasPieceValue = player.HasPiece;
+            var realDistToClosestPiece = player.Board[player.Position.y, player.Position.x].DistToPiece;
+
+            // Assert
+            Assert.Equal(expectedHasPieceValue, realHasPieceValue);
+            Assert.Equal(expectedDistance, realDistToClosestPiece);
         }
     }
 }
