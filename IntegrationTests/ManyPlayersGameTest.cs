5b033c36921cbce75ab1f8c793a1f607b3d99ad1
Przemysław Wiczołek <przemyslaw.wk@gmail.com>
2020-04-18 11:39:04 +0200

    Increase timeout again

--- IntegrationTests/ManyPlayersGameTest.cs
+++ IntegrationTests/ManyPlayersGameTest.cs
@@ -32,7 +32,7 @@ namespace IntegrationTests
             this.positionsCheckTime = 6000;
         }
 
-        [Fact(Timeout = 6 * 60 * 1000)]
+        [Fact(Timeout = 10 * 60 * 1000)]
         public override async void RunGameWithConfiguration()
         {
             await RunGame();
