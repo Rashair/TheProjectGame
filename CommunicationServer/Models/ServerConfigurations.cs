bf5ccb94c6f9a51cb2083c78e3fc47e1153abe42
Zhanna Solobchuk <01135029@pw.edu.pl>
2020-04-18 16:59:03 +0200

    Added and fixed CS configuration

---
+++ CommunicationServer/Models/ServerConfigurations.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CommunicationServer.Models
+{
+    public class ServerConfigurations
+    {
+        public int PortAgentow { get; set; }
+
+        public int PortGM { get; set; }
+    }
+}
