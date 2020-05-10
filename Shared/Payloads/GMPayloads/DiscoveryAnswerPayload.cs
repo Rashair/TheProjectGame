22a86d358d1830ccb463bd82cb1c702ef501cb6b
Bartek <bartek77@live.com>
2020-05-10 22:33:32 +0200

    Added returning null in Discover

--- Shared/Payloads/GMPayloads/DiscoveryAnswerPayload.cs
+++ Shared/Payloads/GMPayloads/DiscoveryAnswerPayload.cs
@@ -2,23 +2,23 @@
 {
     public class DiscoveryAnswerPayload : Payload
     {
-        public int DistanceFromCurrent { get; set; }
+        public int? DistanceFromCurrent { get; set; }
 
-        public int DistanceN { get; set; }
+        public int? DistanceN { get; set; }
 
-        public int DistanceNE { get; set; }
+        public int? DistanceNE { get; set; }
 
-        public int DistanceE { get; set; }
+        public int? DistanceE { get; set; }
 
-        public int DistanceSE { get; set; }
+        public int? DistanceSE { get; set; }
 
-        public int DistanceS { get; set; }
+        public int? DistanceS { get; set; }
 
-        public int DistanceSW { get; set; }
+        public int? DistanceSW { get; set; }
 
-        public int DistanceW { get; set; }
+        public int? DistanceW { get; set; }
 
-        public int DistanceNW { get; set; }
+        public int? DistanceNW { get; set; }
 
         public override string ToString()
         {
