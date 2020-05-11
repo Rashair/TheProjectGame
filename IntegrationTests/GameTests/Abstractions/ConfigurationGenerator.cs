5e812d5a47047476f03a8b87bb1c9873544498fb
Przemysław Wiczołek <przemyslaw.wk@gmail.com>
2020-05-11 10:53:12 +0200

    Disable verbose logging in CS

--- IntegrationTests/GameTests/Abstractions/ConfigurationGenerator.cs
+++ IntegrationTests/GameTests/Abstractions/ConfigurationGenerator.cs
@@ -23,6 +23,7 @@ namespace IntegrationTests.GameTests.Abstractions
                 $"GMPort={csPort}",
                 $"PlayerPort={playerPort}",
                 $"ListenerIP={csIP}",
+                "Verbose=false"
             };
             var gmArgs = new string[]
             {
