1547a72fcbb21963292ceb347e364e9567f84ea2
Anna Szmurło <01130615@pw.edu.pl>
2020-05-11 19:31:54 +0200

    Add new messages

---
+++ Shared/Payloads/GMPayloads/InformationExchangeResponsePayload.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shared.Payloads.GMPayloads
+{
+    public class InformationExchangeResponsePayload
+    {
+        public bool Received { get; set; }
+
+        public override string ToString()
+        {
+            return $"Received: {Received}";
+        }
+    }
+}
