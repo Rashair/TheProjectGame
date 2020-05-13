a730becc1dfdbcb3452fba1e2cddbdf2b11149a3
Anna Szmurło <01130615@pw.edu.pl>
2020-05-13 12:09:01 +0200

    Change Put tests in GMTests

--- GameMasterTests/GMTests.cs
+++ GameMasterTests/GMTests.cs
@@ -101,9 +101,8 @@ namespace GameMaster.Tests
             int x = 3;
             int y = 4;
             NormalPiece piece = new NormalPiece();
-            Mock<GoalField> field = new Mock<GoalField>(x, y);
-            field.Setup(m => m.Put(piece)).Returns((PutEvent.NormalOnGoalField, true));
-            bool result = piece.Put(field.Object).putEvent == PutEvent.NormalOnGoalField ? true : false;
+            GoalField field = new GoalField(x, y);
+            bool result = field.Put(piece).putEvent == PutEvent.NormalOnGoalField ? true : false;
             Assert.True(result);
         }
 
@@ -113,10 +112,9 @@ namespace GameMaster.Tests
             int x = 3;
             int y = 4;
             ShamPiece piece = new ShamPiece();
-            Mock<NonGoalField> field = new Mock<NonGoalField>(x, y);
-            field.Setup(m => m.Put(piece)).Returns((PutEvent.ShamOnGoalArea, true));
-            bool result = piece.Put(field.Object).putEvent == PutEvent.NormalOnGoalField ? true : false;
-            Assert.False(result);
+            NonGoalField field = new NonGoalField(x, y);
+            bool result = field.Put(piece).putEvent == PutEvent.ShamOnGoalArea ? true : false;
+            Assert.True(result);
         }
 
         public class DiscoverTestData : IEnumerable<object[]>
