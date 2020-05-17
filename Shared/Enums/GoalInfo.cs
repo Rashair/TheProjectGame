db309999e3e012b7656bbd7f3bf47c1dc2443596
Przemysław Wiczołek <przemyslaw.wk@gmail.com>
2020-05-17 02:04:55 +0200

    Fix enums converter

--- Shared/Enums/GoalInfo.cs
+++ Shared/Enums/GoalInfo.cs
@@ -1,5 +1,9 @@
-﻿namespace Shared.Enums
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace Shared.Enums
 {
+    [JsonConverter(typeof(StringEnumConverter))]
     public enum GoalInfo
     {
         IDK,
