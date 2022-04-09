8879f42a3fd041b8673efe4b3d3a34b0bc89c4d0
Rashair <przemyslaw.wk@gmail.com>
2022-04-09 10:39:18 +0200

    Update project to .NET 6

--- IntegrationTests/GameTests/Abstractions/GameTest.cs
+++ IntegrationTests/GameTests/Abstractions/GameTest.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Linq;
 using System.Net.Http;
+using System.Net.Http.Json;
 using System.Threading;
 using System.Threading.Tasks;
 
--- IntegrationTests/Utilities.cs
+++ IntegrationTests/Utilities.cs
@@ -4,6 +4,7 @@ using System.Reflection;
 
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
 using Serilog;
 
 namespace IntegrationTests
@@ -19,8 +20,6 @@ namespace IntegrationTests
                 .UseContentRoot(projectDir)
                 .ConfigureAppConfiguration((hostingContext, config) =>
                 {
-                    var env = hostingContext.HostingEnvironment;
-
                     config.AddJsonFile("appsettings.json", optional: false, reloadOnChange: false);
                     config.AddEnvironmentVariables();
                     config.AddCommandLine(args);
